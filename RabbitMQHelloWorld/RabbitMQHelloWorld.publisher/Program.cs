7b34a58671b9efdb44ee2ef1788d99316e58ecc5
Eray Bakır <info@eraybakir.com>
2021-08-04 13:06:52 +0300

    Multiple Data Send

--- RabbitMQHelloWorld/RabbitMQHelloWorld.publisher/Program.cs
+++ RabbitMQHelloWorld/RabbitMQHelloWorld.publisher/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Linq;
 using System.Text;
 
 namespace RabbitMQHelloWorld.publisher
@@ -25,24 +26,30 @@ namespace RabbitMQHelloWorld.publisher
             var channel = connection.CreateModel();
 
             //Mesaj iletimi için kuyruk oluşturduk
+            //Kuyruk daha önce oluşturulmuşsa hata vermez çalışmaya devam eder(parametreler aynı ise)
             //ilk parametre kuyruk adı,ikinci parametre kuyruk saklansın mı
             //üçünü parametre sadece bu kanal üzerinden mi kuyruğa bağlanılsın diye soruyor
             //dördüncü kuyruğa bağlı olan subscriber ların hepsi silinirse kuyruk silinsin mi,
             channel.QueueDeclare("hello-queue", true, false, false);
 
-            //İletilecek ifade
-            string message = "hello world";
+            //For kullanımı gibi çalışır,çoklu data gönderimini test etmek için oluşturduk
+            Enumerable.Range(1, 50).ToList().ForEach(data=> 
+            {
+                //İletilecek ifade
+                string message = $"Message {data}";
 
-            //İletim byte şeklinde olduğu için byte a çevirdik
-            var messageBody = Encoding.UTF8.GetBytes(message);
+                //İletim byte şeklinde olduğu için byte a çevirdik
+                var messageBody = Encoding.UTF8.GetBytes(message);
 
-            //kuyruğa data ekleme
-            //ilk parametre exchange ismi
-            //exchange miz olmadığı için kanalın da hangi kuyruğa mesajı ileteceğine karar verebilmesi için ikinci parametre de kuyruk ismi veriyoruz
-            //Son parametrede mesajımı veriyorum
-            channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+                //kuyruğa data ekleme
+                //ilk parametre exchange ismi
+                //exchange miz olmadığı için kanalın da hangi kuyruğa mesajı ileteceğine karar verebilmesi için ikinci parametre de kuyruk ismi veriyoruz
+                //Son parametrede mesajımı veriyorum
+                channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
 
-            Console.WriteLine("Mesaj gönderilmiştir");
+                Console.WriteLine("Gönderilen Mesaj: "+message);
+            });
+         
             Console.ReadLine();
         }
     }
--- RabbitMQHelloWorld/RabbitMQHelloWorld.subscriber/Program.cs
+++ RabbitMQHelloWorld/RabbitMQHelloWorld.subscriber/Program.cs
@@ -25,17 +25,24 @@ namespace RabbitMQHelloWorld.subscriber
             var channel = connection.CreateModel();
 
             //Mesaj alımı için kuyruk oluşturduk
+            //Kuyruk daha önce oluşturulmuşsa hata vermez çalışmaya devam eder(parametreler aynı ise)
             //publisher ve subscriber tarafında oluşturulan kuyrukların özellikleri aynı olmalıdır
             //ilk parametre kuyruk adı,ikinci parametre kuyruk saklansın mı
             //üçünü parametre sadece bu kanal üzerinden mi kuyruğa bağlanılsın diye soruyor
             //dördüncü kuyruğa bağlı olan subscriber ların hepsi silinirse kuyruk silinsin mi,
             channel.QueueDeclare("hello-queue", true, false, false);
 
+            //subscriber lara kaçar adet data göndereceğimizi belirliyoruz.
+            //ilk parametrenin 0 olması herhangi bir boyutta ki mesajı alabiliriz demek
+            //ikinci parametre kaçar adet mesaj gönderileceğini belirliyor
+            //üçüncü parametre  true ise mesajlar 1 er 1er dağılatacak false ise toplamı 1 olarak şekilde dağıtılacak örneğin 5 değerini seçseydik hepsine teker teker vererek toplamı 5 yapmaya çalışacak sonra yine tur a devam edecekti.
+            channel.BasicQos(0, 1, false);
 
             var subscriber = new EventingBasicConsumer(channel);
 
             //ilk parametreye kuyruk ismi tanımladık
             //ikinci parametre iletim sonrası mesaj silinsin mi diye soruyor
+            //ikinci parametreye false verdiysek eğer ileride rabbitmq ya mesajın silinmesi için haber verebiliriz
             channel.BasicConsume("hello-queue", false, subscriber);
 
             //Eventi yakalıyoruz
@@ -44,6 +51,10 @@ namespace RabbitMQHelloWorld.subscriber
                 //Mesaj byte array olarak gegldiği için çevrim işlemi yapıyoruz
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
                 Console.WriteLine("Gelen Mesaj : " + message);
+
+                //mesajın iletildikten sonra silinmemesini seçtiysek bu kısımda artık mesajımızı silebiliriz
+                //e.delivertag mesaj bilgisini alıyor,ikinci parametre ise işlenmiş ama rabbitmq ya gitmemiş mesajlar da dahil edilsin mi diye soruyor
+                channel.BasicAck(e.DeliveryTag, false);
             };
 
 
